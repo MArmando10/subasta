b30181de4457a2d0fcd0a8719ab85888cf92db56
LuisA1 <proffine1@gmail.com>
2020-06-22 12:37:25 -0500

    Migraciones Basicas/Seeders Admin/User

---
+++ database/migrations/2020_06_22_164845_create_products_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('category_id');
+            $table->string('name');
+            $table->bigInteger('reputation')->nullable();
+            $table->string('city');
+            $table->string('country');
+            //nombre
+            //reputacion
+            //time
+            //ciudad
+            //pais
+            //tipos de envio
+            //ofertas aplicables
+            //ranking ofertas
+            //favoritos - creo que este es una tabla/ productos favoritos
+            //devoluciones
+            //imagenes
+            //metadata
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $user = new User();
+        $user->name = "Admin";
+        $user->email = "admin@admin.com";
+        $user->password = Hash::make('admin');
+        $user->is_admin = true;
+        $user->save();
+    }
+}

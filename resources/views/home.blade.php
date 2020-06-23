e62c68e68f0d3055be6907b345bf228204e67478
LuisA1 <proffine1@gmail.com>
2020-06-23 12:30:54 -0500

    installing laravel collective to product form security

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
+
+<div class="">
+    <div class="bg-secondary">
+        <div>
+            <h4>Mejores Precios</h4>
+        </div>
+        <div class="container-sm">
+            <img src="{{ asset('img/pruebas/headphones.jpg') }}" class="rounded" alt="...">
+        </div>
+
+    </div>
+    
+    <div>
+    
+    </div>
+    
+    <div>
+    
+    </div>
+</div>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
